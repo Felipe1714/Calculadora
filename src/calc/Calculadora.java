f0adcfb8814cf2e6e49dd1b3f717de25197397ad
Felipe1714 <feps87@gmail.com>
2016-05-10 20:48:03 -0300

    Teste de Calculadora

---
+++ src/calc/Calculadora.java
@@ -0,0 +1,27 @@
+package calc;
+
+public class Calculadora {
+	
+	public static void main(String[] args) {
+		
+	double param1, param2;
+	
+	public double Somar(param1, param2);
+	
+	return param1 + param2;
+	
+	public double Subtrair(param1, param2);
+	
+	return param1 - param2;
+	
+	public double Multiplicar(param1, param2);
+	
+	return param1 * param2;
+			
+	public double Dividir(param1, param2);
+	
+	return param1 / param2;
+	
+	}
+
+}
