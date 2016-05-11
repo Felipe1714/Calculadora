a85ad237920f8aac49e7ee94e0cb6edd53e83ca6
Felipe1714 <feps87@gmail.com>
2016-05-10 21:12:20 -0300

    Calculadora 1.0.0

--- src/calc/Calculadora.java
+++ src/calc/Calculadora.java
@@ -1,22 +1,32 @@
 package calc;
 
 public class Calculadora {
+	
+	// Versão 1.0.0
 
 	public static void main(String[] args) {
 
 		Calculadora calc = new Calculadora();
 		
 		double resultado = calc.Somar(32, 84);
+		System.out.println("Resultado da Soma (32 + 84)");
 		System.out.println(resultado);
+		System.out.println();
 		
 		resultado = calc.Subtrair(32, 8);
+		System.out.println("Resultado da Subtração (32 - 8)");
 		System.out.println(resultado);
+		System.out.println();
 		
 		resultado = calc.Multiplicar(3, 8);
+		System.out.println("Resultado da Multiplicação (3 * 8)");
 		System.out.println(resultado);
+		System.out.println();
 		
 		resultado = calc.Dividir(32, 8);
+		System.out.println("Resultado da Divisão (32 / 8)");
 		System.out.println(resultado);
+		System.out.println();
 	}
 
 	public double Somar(double param1, double param2){
