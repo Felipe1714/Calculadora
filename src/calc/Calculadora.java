1f01366693ec3a96aa6d69ba52e2eed218d4ec04
Felipe1714 <feps87@gmail.com>
2016-05-10 21:02:08 -0300

    Calculadora Funcionando

--- src/calc/Calculadora.java
+++ src/calc/Calculadora.java
@@ -1,27 +1,45 @@
 package calc;
 
 public class Calculadora {
-	
+
 	public static void main(String[] args) {
+
+		Calculadora calc = new Calculadora();
 		
-	double param1, param2;
-	
-	public double Somar(param1, param2);
-	
-	return param1 + param2;
-	
-	public double Subtrair(param1, param2);
-	
-	return param1 - param2;
-	
-	public double Multiplicar(param1, param2);
-	
-	return param1 * param2;
-			
-	public double Dividir(param1, param2);
-	
-	return param1 / param2;
-	
+		double resultado = calc.Somar(32, 84);
+		System.out.println(resultado);
+		
+		resultado = calc.Subtrair(32, 8);
+		System.out.println(resultado);
+		
+		resultado = calc.Multiplicar(3, 8);
+		System.out.println(resultado);
+		
+		resultado = calc.Dividir(32, 8);
+		System.out.println(resultado);
 	}
 
+	public double Somar(double param1, double param2){
+
+		return param1 + param2;
+
+	}
+
+	public double Subtrair(double param1, double param2){
+
+		return param1 - param2;
+
+	}
+
+	public double Multiplicar(double param1, double  param2){
+
+		return param1 * param2;
+
+	}
+
+	public double Dividir(double param1, double param2){
+
+		return param1 / param2;
+
+	}
 }
